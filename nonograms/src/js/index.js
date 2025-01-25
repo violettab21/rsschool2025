b8d84e994563917d80873edbb296ac87e6b32ffc
Viyaleta Batsura <violettabatsura@gmail.com>
2025-01-25 19:27:26 +0300

    init: start Nonograms task, create project structure

---
+++ nonograms/src/js/index.js
@@ -0,0 +1,4 @@
+import "../../index.html";
+import "../sass/_style.scss";
+
+console.log("Hello Nonograms");
---
+++ nonograms/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+
+const mode = "production";
+
+const devtool = "source-map";
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  mode,
+  devtool,
+  devServer: {
+    open: true,
+  },
+  entry: "./src/js/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.[contenthash].js",
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "main.[contenthash].css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
