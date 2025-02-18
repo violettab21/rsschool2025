c51fa8c1ad1224859c55512434035f4048911741
Viyaleta Batsura <violettabatsura@gmail.com>
2025-02-18 22:35:50 +0300

    feat: configure Webpack to collaborate with TS

--- news-api/webpack.config.js
+++ news-api/webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const DotenvWebpackPlugin = require('dotenv-webpack');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index'),
     mode: 'development',
     module: {
         rules: [
@@ -13,10 +13,15 @@ const baseConfig = {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.ts$/i,
+                use: 'ts-loader',
+                include: [path.resolve(__dirname, 'src')],
+            },
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.ts', '.js'],
     },
     output: {
         filename: 'index.js',
