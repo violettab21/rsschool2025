0f5c142ba831b80dff13d56fee84bcd6ac041b9e
Viyaleta Batsura <violettabatsura@gmail.com>
2025-02-21 00:14:05 +0300

    refactor: add types for News and Sources classes

--- news-api/src/components/view/news/news.ts
+++ news-api/src/components/view/news/news.ts
@@ -1,18 +1,18 @@
 import './news.css';
-
+import { Article } from '../../../interfaces';
 class News {
-    draw(data) {
+    draw(data: Article[]) {
         const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
 
-        const fragment = document.createDocumentFragment();
-        const newsItemTemp = document.querySelector('#newsItemTemp');
+        const fragment: Node = document.createDocumentFragment();
+        const newsItemTemp: HTMLTemplateElement = document.querySelector('#newsItemTemp');
 
-        news.forEach((item, idx) => {
-            const newsClone = newsItemTemp.content.cloneNode(true);
+        news.forEach((item: Article, idx: number) => {
+            const newsClone = newsItemTemp.content.cloneNode(true) as HTMLElement;
 
             if (idx % 2) newsClone.querySelector('.news__item').classList.add('alt');
 
-            newsClone.querySelector('.news__meta-photo').style.backgroundImage = `url(${
+            (newsClone.querySelector('.news__meta-photo') as HTMLElement).style.backgroundImage = `url(${
                 item.urlToImage || 'img/news_placeholder.jpg'
             })`;
             newsClone.querySelector('.news__meta-author').textContent = item.author || item.source.name;
@@ -27,7 +27,7 @@ class News {
             newsClone.querySelector('.news__description-content').textContent = item.description;
             newsClone.querySelector('.news__read-more a').setAttribute('href', item.url);
 
-            fragment.append(newsClone);
+            fragment.appendChild(newsClone);
         });
 
         document.querySelector('.news').innerHTML = '';
--- news-api/src/components/view/sources/sources.ts
+++ news-api/src/components/view/sources/sources.ts
@@ -1,17 +1,17 @@
 import './sources.css';
-
+import { Source } from '../../../interfaces';
 class Sources {
-    draw(data) {
-        const fragment = document.createDocumentFragment();
-        const sourceItemTemp = document.querySelector('#sourceItemTemp');
+    draw(data: Source[]) {
+        const fragment = document.createDocumentFragment() as Node;
+        const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
 
-        data.forEach((item) => {
-            const sourceClone = sourceItemTemp.content.cloneNode(true);
+        data.forEach((item: Source) => {
+            const sourceClone = sourceItemTemp.content.cloneNode(true) as Element;
 
             sourceClone.querySelector('.source__item-name').textContent = item.name;
             sourceClone.querySelector('.source__item').setAttribute('data-source-id', item.id);
 
-            fragment.append(sourceClone);
+            fragment.appendChild(sourceClone);
         });
 
         document.querySelector('.sources').append(fragment);
