e10127fa1f99994f6c752f53cf611e68ab6ec1e6
Viyaleta Batsura <violettabatsura@gmail.com>
2025-02-20 22:01:28 +0300

    refactor: add Interfaces for Sources and Articles

---
+++ news-api/src/interfaces.ts
@@ -0,0 +1,42 @@
+interface Source {
+    id: string;
+    name: string;
+    description: string;
+    url: string;
+    category: string;
+    language: string;
+    country: string;
+}
+
+interface Article {
+    source: {
+        id?: string;
+        name: string;
+    };
+    author?: string;
+    title: string;
+    description?: string;
+    url: string;
+    urlToImage?: string;
+    publishedAt: string;
+    content: string;
+}
+
+interface EverythingResponse {
+    status: 'ok' | 'error';
+    code?: string;
+    message?: string;
+    totalResults?: number;
+    articles?: Article[];
+}
+
+interface SourceResponse {
+    status: 'ok' | 'error';
+    code?: string;
+    message?: string;
+    sources?: Source[];
+}
+
+interface Options {
+
+}
