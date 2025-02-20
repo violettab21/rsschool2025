bd2b7eb60ea25f9327e75f51257b7d78c9df49dc
Viyaleta Batsura <violettabatsura@gmail.com>
2025-02-20 23:29:55 +0300

    refactor: add TS types for AppController class

--- news-api/src/components/controller/controller.ts
+++ news-api/src/components/controller/controller.ts
@@ -1,7 +1,7 @@
 import AppLoader from './appLoader';
-
+import { EverythingResponse, SourceResponse } from '../../interfaces';
 class AppController extends AppLoader {
-    getSources(callback) {
+    getSources(callback: (data: EverythingResponse | SourceResponse) => void): void {
         super.getResp(
             {
                 endpoint: 'sources',
@@ -10,13 +10,13 @@ class AppController extends AppLoader {
         );
     }
 
-    getNews(e, callback) {
-        let target = e.target;
-        const newsContainer = e.currentTarget;
+    getNews(e: Event, callback: (data: EverythingResponse | SourceResponse) => void): void {
+        let target: Element = e.target as Element;
+        const newsContainer: Element = e.currentTarget as Element;
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
-                const sourceId = target.getAttribute('data-source-id');
+                const sourceId: string = target.getAttribute('data-source-id');
                 if (newsContainer.getAttribute('data-source') !== sourceId) {
                     newsContainer.setAttribute('data-source', sourceId);
                     super.getResp(
@@ -31,7 +31,7 @@ class AppController extends AppLoader {
                 }
                 return;
             }
-            target = target.parentNode;
+            target = target.parentNode as Element;
         }
     }
 }
