e443f70c0795849a686423ec5aba748d0f7d345e
Viyaleta Batsura <violettabatsura@gmail.com>
2025-03-12 00:42:47 +0300

    refactor: set up Prettier, ESlint

---
+++ decision-making-tool/eslint.config.js
@@ -0,0 +1,56 @@
+const globals = require('globals');
+const pluginJs = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const prettier = require('prettier');
+const eslintPluginUnicorn = require('eslint-plugin-unicorn');
+
+/** @type {import('eslint').Linter.Config[]} */
+module.exports = [
+    { files: ['**/*.{js,mjs,cjs,ts}'] },
+    {
+        languageOptions: {
+            globals: globals.browser,
+            parser: '@typescript-eslint/parser',
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: __dirname,
+            },
+        },
+    },
+    {
+        rules: {
+            'no-debugger': 'off',
+            'no-console': 0,
+            'class-methods-use-this': 'off',
+            '@typescript-eslint/no-explicit-any': 'error',
+            '@typescript-eslint/consistent-type-assertions': [
+                'error',
+                { assertionStyle: 'never' },
+            ],
+            '@typescript-eslint/consistent-type-imports': 'error',
+            '@typescript-eslint/explicit-function-return-type': 'error',
+            '@typescript-eslint/explicit-member-accessibility': [
+                'error',
+                {
+                    accessibility: 'explicit',
+                    overrides: { constructors: 'off' },
+                },
+            ],
+            '@typescript-eslint/member-ordering': 'error',
+            'class-methods-use-this': 'error',
+            '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+            'unicorn/better-regex': 'error',
+            'unicorn/no-empty-file': 'error',
+            'unicorn/no-useless-undefined': 'error',
+        },
+    },
+    {
+        plugins: {
+            prettier: prettier,
+            tseslint: tseslint,
+            unicorn: eslintPluginUnicorn,
+        },
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+];
