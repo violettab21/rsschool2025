db1d39c3ef8683c9ddea4021713cc27f43fb21d3
Viyaleta Batsura <violettabatsura@gmail.com>
2025-03-11 22:18:18 +0300

    init: start decision-making-tool task, set up webpack and TS

---
+++ decision-making-tool/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require("path");
+
+const mode = "production";
+
+const devtool = "source-map";
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  mode,
+  devtool,
+  devServer: {
+    open: true,
+  },
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.[contenthash].js",
+    clean: true,
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/index.html"),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "main.[contenthash].css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.ts$/i,
+        use: "ts-loader",
+        include: [path.resolve(__dirname, "src")],
+      },
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: { mode: "icss" },
+            },
+          },
+          "sass-loader",
+        ],
+      },
+
+      { test: /\.jpe?g$|\.svg$|\.png$|\.ico$|\.mp3$/, use: ["file-loader"] },
+    ],
+  },
+};
